23a3ec1bb5a43459bac490c799205f511811a0af
Chris James <quii@hey.com>
2023-07-15 07:39:47 +0100

    smoke test for calculateRounds

--- src/domain/scores.test.js
+++ src/domain/scores.test.js
@@ -1,5 +1,5 @@
-import {calculateGoldCount, calculateHitsCount, calculateTotal, validScores} from "./scores";
-import { test, expect } from 'vitest'
+import { calculateGoldCount, calculateHitsCount, calculateRounds, calculateTotal, validScores } from "./scores";
+import { test, expect, describe } from 'vitest'
 
 // note to ruth: https://vitest.dev/
 test("it knows the valid scores of imperial archery", () => {
@@ -42,4 +42,14 @@ test.each([
     [[1, 3, 5, "M", 1, 7], 0]
 ])("it can calculate golds", (scores, expectedGoldsCount) => {
     expect(calculateGoldCount(scores)).toEqual(expectedGoldsCount);
+});
+
+describe('calculateRounds', () => {
+    test('snapshot test of ruths first competition', () => {
+        const scores = [9,9,7,7,3,3,9,9,7,5,5,1,9,7,7,7,5,5,7,7,7,5,5,1,9,7,7,3,3,1,9,9,7,7,5,3,9,7,7,5,5,5,9,9,9,7,7,7,9,9,9,9,7,7,9,9,9,9,9,7,9,7,7,7,7,5,9,7,7,7,7,3,9,9,9,9,7,7,9,9,9,9,9,9,9,9,9,9,9,7,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9];
+        const gameType = 'windsor';
+        const result = calculateRounds(scores, gameType);
+
+        expect(JSON.stringify(result, null, 2)).toMatchFileSnapshot('./__snapshots__/ruths_first_competition.json');
+    });
 });
