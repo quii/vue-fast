63baa3e35c888d452d31306ab7c44f219cf7fcff
Chris James <quii@hey.com>
2023-07-29 20:10:44 +0100

    fixes #20

--- src/domain/topscores.js
+++ src/domain/topscores.js
@@ -2,27 +2,32 @@ export function addTopScoreIndicator(scoringHistory) {
     // Create a copy of the input object to avoid modifying the original data
     const scoringHistoryCopy = JSON.parse(JSON.stringify(scoringHistory));
 
-    // Create an object to store the best scores for each distance
-    const bestScoresByDistance = {};
+    // Create an object to store the best scores for each game type at each distance
+    const bestScoresByDistanceAndGameType = {};
 
-    // Loop through the scoring history data to find the best scores for each distance
+    // Loop through the scoring history data to find the best scores for each distance and game type
     for (const entry of scoringHistoryCopy) {
         const distance = entry.distance;
+        const gameType = entry.gameType;
         const score = entry.score;
 
-        // If the distance key doesn't exist in the object or the current score is higher, update the value
-        if (!(distance in bestScoresByDistance) || score > bestScoresByDistance[distance]) {
-            bestScoresByDistance[distance] = score;
+        if (!bestScoresByDistanceAndGameType[distance]) {
+            bestScoresByDistanceAndGameType[distance] = {};
+        }
+
+        if (!bestScoresByDistanceAndGameType[distance][gameType] || score > bestScoresByDistanceAndGameType[distance][gameType]) {
+            bestScoresByDistanceAndGameType[distance][gameType] = score;
         }
     }
 
     // Loop through the scoring history data and add the 'topScore' boolean field
     for (const entry of scoringHistoryCopy) {
         const distance = entry.distance;
+        const gameType = entry.gameType;
         const score = entry.score;
 
-        // Check if the current score is the best score for this distance
-        entry.topScore = score === bestScoresByDistance[distance];
+        // Check if the current score is the best score for this distance and game type
+        entry.topScore = score === bestScoresByDistanceAndGameType[distance][gameType];
     }
 
     return scoringHistoryCopy;
--- src/domain/topscores.test.js
+++ src/domain/topscores.test.js
@@ -1,17 +1,75 @@
-import { test, expect} from "vitest";
-import {addTopScoreIndicator} from "@/domain/topscores";
+import { describe, test, expect } from "vitest";
+import { addTopScoreIndicator } from "@/domain/topscores";
 
-test('addTopScoreIndicator', () => {
+describe("addTopScoreIndicator", () => {
+  test("happy path", () => {
     const scoringHistory = [
-        {"id": 1, "date": "2023-07-21", "score": 299, "distance": 40, "gameType": "warwick", "scores": [/* scores here */]},
-        {"id": 2, "date": "2023-07-20", "score": 242, "distance": 40, "gameType": "warwick", "scores": [/* scores here */]},
-        {"id": 3, "date": "2023-07-18", "score": 242, "distance": 30, "gameType": "warwick", "scores": [/* scores here */]}
+      {
+        "id": 1,
+        "date": "2023-07-21",
+        "score": 299,
+        "distance": 40,
+        "gameType": "warwick",
+        "scores": [/* scores here */]
+      },
+      {
+        "id": 2,
+        "date": "2023-07-20",
+        "score": 242,
+        "distance": 40,
+        "gameType": "warwick",
+        "scores": [/* scores here */]
+      },
+      {
+        "id": 3,
+        "date": "2023-07-18",
+        "score": 242,
+        "distance": 30,
+        "gameType": "warwick",
+        "scores": [/* scores here */]
+      }
     ];
 
     const scoringHistoryWithTopScores = addTopScoreIndicator(scoringHistory);
 
-    expect(scoringHistoryWithTopScores).toHaveLength(3)
-    expect(scoringHistoryWithTopScores[0].topScore).toBeTruthy()
-    expect(scoringHistoryWithTopScores[1].topScore).toBeFalsy()
-    expect(scoringHistoryWithTopScores[2].topScore).toBeTruthy()
+    expect(scoringHistoryWithTopScores).toHaveLength(3);
+    expect(scoringHistoryWithTopScores[0].topScore).toBeTruthy();
+    expect(scoringHistoryWithTopScores[1].topScore).toBeFalsy();
+    expect(scoringHistoryWithTopScores[2].topScore).toBeTruthy();
+  });
+
+  test("groups top scores by game type", () => {
+    const scoringHistoryInDifferentGameTypes = [
+      {
+        "id": 1,
+        "date": "2023-07-21",
+        "score": 299,
+        "distance": 40,
+        "gameType": "warwick",
+        "scores": [/* scores here */]
+      },
+      {
+        "id": 2,
+        "date": "2023-07-20",
+        "score": 242,
+        "distance": 40,
+        "gameType": "warwick",
+        "scores": [/* scores here */]
+      },
+      {
+        "id": 3,
+        "date": "2023-07-18",
+        "score": 242,
+        "distance": 40,
+        "gameType": "national",
+        "scores": [/* scores here */]
+      }
+    ];
+    const scoringHistoryWithTopScores = addTopScoreIndicator(scoringHistoryInDifferentGameTypes);
+    expect(scoringHistoryWithTopScores).toHaveLength(3);
+    expect(scoringHistoryWithTopScores[0].topScore).toBeTruthy();
+    expect(scoringHistoryWithTopScores[1].topScore).toBeFalsy();
+    expect(scoringHistoryWithTopScores[2].topScore).toBeTruthy();
+
+  });
 });
