f2a58dc4e5e3c0adcf7617e9894b5e09b783eb28
Chris James <quii@hey.com>
2024-04-25 07:41:52 +0100

    fix loading historical games

--- src/ViewShoot.vue
+++ src/ViewShoot.vue
@@ -2,6 +2,7 @@
 import RoundScores from '@/components/RoundScores.vue'
 import { useRoute } from 'vue-router'
 import { useHistoryStore } from '@/stores/history'
+import { gameTypeConfig } from "@/domain/game_types";
 
 const route = useRoute()
 const scores = useHistoryStore()
@@ -11,7 +12,9 @@ scores.setShootToView(route.params.id)
 
 <template>
   <h1>{{ scores.selectedShoot.gameType }} - {{ scores.selectedShoot.date }}</h1>
-  <RoundScores :scores="scores.selectedShoot.scores" :game-type="scores.selectedShoot.gameType" />
+  <RoundScores :scores="scores.selectedShoot.scores"
+               :end-size="gameTypeConfig[scores.selectedShoot.gameType].endSize"
+               :game-type="scores.selectedShoot.gameType" />
 </template>
 
 <style scoped>
