5d53d100e05809cfe1f2267e30c848bfb5c50752
Chris James <quii@hey.com>
2024-04-26 11:23:57 +0100

    fix snapshot

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.li0aifpqsm"
+    "revision": "0.8aeo7n5vmk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
