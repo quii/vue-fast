b361753d27d7dff4dc6f788530d4f2b03ee611f9
Chris James <quii@hey.com>
2024-05-06 15:11:08 +0100

    test for Mally's broken data scenario

--- cypress/e2e/data_management.cy.js
+++ cypress/e2e/data_management.cy.js
@@ -1,4 +1,4 @@
-import { cjSave } from "../../src/domain/test_data";
+import { cjSave, mallySave } from "../../src/domain/test_data";
 
 describe("data management", () => {
   it("import cj save and view history", () => {
@@ -12,5 +12,19 @@ describe("data management", () => {
     cy.get("a").contains("History").click();
     cy.get("td").contains("Sat, 20 Apr 2024").click();
     cy.get("[data-test=\"totalGolds\"]").contains("19");
+  });
+
+  it("import Mally's game, view history", () => {
+    cy.visit("/");
+    cy.get("a").contains("Data").click();
+    cy.get("button").contains("Reset").click();
+
+    const rawMallyData = JSON.stringify(mallySave);
+    cy.get("textarea").type(rawMallyData, { parseSpecialCharSequences: false, delay: 0 });
+    cy.get("button").contains("Import").click();
+    cy.visit("/"); //let the data fixer do its thing
+    cy.get("a").contains("History").click();
+    cy.get("td").contains("Sun, 5 May 2024").click();
+    cy.get("[data-test=\"totalGolds\"]").contains("9");
   });
 });
