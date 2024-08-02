c90553e29446bf5af29d81f98883c4d7cdd17bec
Chris James <quii@hey.com>
2024-08-02 16:04:37 +0100

    derp

---
+++ src/components/RoundDetails.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  rounds: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <table>
+    <thead>
+    <tr>
+      <th>Round name</th>
+      <th>Number of ends</th>
+      <th>Max Distance</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="round in rounds" :key="round">
+      <td>{{ round.round }}</td>
+      <td>{{ round.numberOfEnds }}</td>
+      <td>{{ round.distance }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+td {
+  text-transform: capitalize;
+}
+</style>
