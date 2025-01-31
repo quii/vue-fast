8c969116f66babf533acb1e5d9c07ca08e401779
Chris James <quii@hey.com>
2025-01-31 14:50:34 +0000

    data entry of distances for each round, i think

--- src/domain/game_type_config.js
+++ src/domain/game_type_config.js
@@ -26,154 +26,176 @@ export const baseConfig = [
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [6, 4, 2],
-    maxDistanceYards: 100
+    maxDistanceYards: 100,
+    otherDistancesYards: [80, 60]
   },
   {
     name: 'hereford / bristol i',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [6, 4, 2],
-    maxDistanceYards: 80
+    maxDistanceYards: 80,
+    otherDistancesYards: [60, 50]
   },
   {
     name: 'bristol ii',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [6, 4, 2],
-    maxDistanceYards: 60
+    maxDistanceYards: 60,
+    otherDistancesYards: [50, 40]
   },
   {
     name: 'bristol iii',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [6, 4, 2],
-    maxDistanceYards: 50
+    maxDistanceYards: 50,
+    otherDistancesYards: [40, 30]
   },
   {
     name: 'bristol iv',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [6, 4, 2],
-    maxDistanceYards: 40
+    maxDistanceYards: 40,
+    otherDistancesYards: [30, 20]
   },
   {
     name: 'bristol v',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [6, 4, 2],
-    maxDistanceYards: 30
+    maxDistanceYards: 30,
+    otherDistancesYards: [20, 10]
   },
   {
     name: 'st. george',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [3, 3, 3],
-    maxDistanceYards: 100
+    maxDistanceYards: 100,
+    otherDistancesYards: [80, 60]
   },
   {
     name: 'albion / long windsor',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [3, 3, 3],
-    maxDistanceYards: 80
+    maxDistanceYards: 80,
+    otherDistancesYards: [60, 50]
   },
   {
     name: 'windsor',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [3, 3, 3],
-    maxDistanceYards: 60
+    maxDistanceYards: 60,
+    otherDistancesYards: [50, 40]
   },
   {
     name: 'windsor 50',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [3, 3, 3],
-    maxDistanceYards: 50
+    maxDistanceYards: 50,
+    otherDistancesYards: [40, 30]
   },
   {
     name: 'windsor 40',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [3, 3, 3],
-    maxDistanceYards: 40
+    maxDistanceYards: 40,
+    otherDistancesYards: [30, 20]
   },
   {
     name: 'windsor 30',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [3, 3, 3],
-    maxDistanceYards: 30
+    maxDistanceYards: 30,
+    otherDistancesYards: [20, 10]
   },
   {
     name: 'new western',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 4],
-    maxDistanceYards: 100
+    maxDistanceYards: 100,
+    otherDistancesYards: [80]
   },
   {
     name: 'long western',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 4],
-    maxDistanceYards: 80
+    maxDistanceYards: 80,
+    otherDistancesYards: [60]
   },
   {
     name: 'western',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 4],
-    maxDistanceYards: 60
+    maxDistanceYards: 60,
+    otherDistancesYards: [50]
   },
   {
     name: 'western 50',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 4],
-    maxDistanceYards: 50
+    maxDistanceYards: 50,
+    otherDistancesYards: [40]
   },
   {
     name: 'western 40',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 4],
-    maxDistanceYards: 40
+    maxDistanceYards: 40,
+    otherDistancesYards: [30]
   },
   {
     name: 'western 30',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 4],
-    maxDistanceYards: 30
+    maxDistanceYards: 30,
+    otherDistancesYards: [20]
   },
   {
     name: 'american',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [2.5, 2.5, 2.5],
-    maxDistanceYards: 60
+    maxDistanceYards: 60,
+    otherDistancesYards: [50, 40]
   },
   {
     name: 'st. nicholas',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 3],
-    maxDistanceYards: 40
+    maxDistanceYards: 40,
+    otherDistancesYards: [30]
   },
   {
     name: 'new national',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 2],
-    maxDistanceYards: 100
+    maxDistanceYards: 100,
+    otherDistancesYards: [80]
   },
   {
     name: 'long national',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 2],
-    maxDistanceYards: 80
+    maxDistanceYards: 80,
+    otherDistancesYards: [60]
   },
   {
     name: 'national',
@@ -188,175 +210,200 @@ export const baseConfig = [
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 2],
-    maxDistanceYards: 50
+    maxDistanceYards: 50,
+    otherDistancesYards: [40]
   },
   {
     name: 'national 40',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 2],
-    maxDistanceYards: 40
+    maxDistanceYards: 40,
+    otherDistancesYards: [30]
   },
   {
     name: 'national 30',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [4, 2],
-    maxDistanceYards: 30
+    maxDistanceYards: 30,
+    otherDistancesYards: [20]
   },
   {
     name: 'new warwick',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [2, 2],
-    maxDistanceYards: 100
+    maxDistanceYards: 100,
+    otherDistancesYards: [80]
   },
   {
     name: 'long warwick',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [2, 2],
-    maxDistanceYards: 80
+    maxDistanceYards: 80,
+    otherDistancesYards: [60]
   },
   {
     name: 'warwick',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [2, 2],
-    maxDistanceYards: 60
+    maxDistanceYards: 60,
+    otherDistancesYards: [50]
   },
   {
     name: 'warwick 50',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [2, 2],
-    maxDistanceYards: 50
+    maxDistanceYards: 50,
+    otherDistancesYards: [40]
   },
   {
     name: 'warwick 40',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [2, 2],
-    maxDistanceYards: 40
+    maxDistanceYards: 40,
+    otherDistancesYards: [30]
   },
   {
     name: 'warwick 30',
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [2, 2],
-    maxDistanceYards: 30
+    maxDistanceYards: 30,
+    otherDistancesYards: [20]
   },
   {
     name: 'wa 1440 (90m)',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3, 3, 3],
-    maxDistanceMetres: 90
+    maxDistanceMetres: 90,
+    otherDistancesMetres: [70]
   },
   {
     name: 'wa 1440 (70m) / metric i',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3, 3, 3],
-    maxDistanceMetres: 70
+    maxDistanceMetres: 70,
+    otherDistancesMetres: [60]
   },
   {
     name: 'wa 1440 (60m) / metric ii',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3, 3, 3],
-    maxDistanceMetres: 60
+    maxDistanceMetres: 60,
+    otherDistancesMetres: [50]
   },
   {
     name: 'metric iii',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3, 3, 3],
-    maxDistanceMetres: 50
+    maxDistanceMetres: 50,
+    otherDistancesMetres: [40]
   },
   {
     name: 'metric iv',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3, 3, 3],
-    maxDistanceMetres: 40
+    maxDistanceMetres: 40,
+    otherDistancesMetres: [30]
   },
   {
     name: 'metric v',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3, 3, 3],
-    maxDistanceMetres: 30
+    maxDistanceMetres: 30,
+    otherDistancesMetres: [20]
   },
   {
     name: 'long metric (men)',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 90
+    maxDistanceMetres: 90,
+    otherDistancesMetres: [70]
   },
   {
     name: 'long metric (women) / long metric i',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 70
+    maxDistanceMetres: 70,
+    otherDistancesMetres: [60]
   },
   {
     name: 'long metric ii',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 60
+    maxDistanceMetres: 60,
+    otherDistancesMetres: [50]
   },
   {
     name: 'long metric iii',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 50
+    maxDistanceMetres: 50,
+    otherDistancesMetres: [40]
   },
   {
     name: 'long metric iv',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 40
+    maxDistanceMetres: 40,
+    otherDistancesMetres: [30]
   },
   {
     name: 'long metric v',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 30
+    maxDistanceMetres: 30,
+    otherDistancesMetres: [20]
   },
   {
     name: 'short metric / short metric i',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 50
+    maxDistanceMetres: 50,
+    otherDistancesMetres: [40]
   },
   {
     name: 'short metric ii',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 40
+    maxDistanceMetres: 40,
+    otherDistancesMetres: [30]
   },
   {
     name: 'short metric iii',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 30
+    maxDistanceMetres: 30,
+    otherDistancesMetres: [20]
   },
   {
     name: 'short metric iv',
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 20
+    maxDistanceMetres: 20,
+    otherDistancesMetres: [10]
   },
   {
     name: 'wa standard bow',
@@ -370,7 +417,8 @@ export const baseConfig = [
     isOutdoor: true,
     isImperial: false,
     distancesRoundSizes: [2.5, 2.5, 2.5],
-    maxDistanceMetres: 60
+    maxDistanceMetres: 60,
+    otherDistancesMetres: [50, 40]
   },
   {
     name: 'wa 70m',
@@ -433,7 +481,8 @@ export const baseConfig = [
     isOutdoor: true,
     isImperial: true,
     distancesRoundSizes: [3, 3],
-    maxDistanceMetres: 15
+    maxDistanceMetres: 15,
+    otherDistancesMetres: [10]
   },
   {
     name: "frostbite",
