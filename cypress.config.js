6eb4bdce88820931bf3ab237e48b67c4cbcafe19
Chris James <quii@hey.com>
2025-04-07 07:50:02 +0100

    fudging live tests to make them less flaky, maybe

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,12 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:4173"
+    baseUrl: "http://localhost:4173",
+    defaultCommandTimeout: 10000,
+    retries: {
+      runMode: 2,
+      openMode: 0
+    }
   },
   chromeWebSecurity: false // https://github.com/cypress-io/cypress/issues/27501
 });
