f9d80b43e81cd5722e52622c2fa37d536d50db80
Chris James <quii@hey.com>
2025-05-04 16:32:14 +0100

    Move routing into its own file

--- src/main.ts
+++ src/main.ts
@@ -1,83 +1,20 @@
 import './assets/main.css'
-import * as VueRouter from 'vue-router'
 
 import { createApp } from 'vue/dist/vue.esm-bundler'
 import DevTools from '@/components/DevTools.vue'
 import { createPinia } from 'pinia'
-import ScoreCard from './ScoreCard.vue'
-import Toast from 'vue-toastification'
+import Toast, { useToast } from 'vue-toastification'
 import InstallBanner from './components/InstallBanner.vue'
 
 import 'vue-toastification/dist/index.css'
-import { registerSW } from "virtual:pwa-register";
-import DataMenuItem from "@/components/DataMenuItem.vue";
-import MainNavigation from "./components/UserNavigation.vue";
-import { useToast } from "vue-toastification";
+import { registerSW } from 'virtual:pwa-register'
+import MainNavigation from './components/UserNavigation.vue'
 import { useInstallationStore } from './stores/installation'
 import { backupService } from './services/backupService'
+import { createRouter, routes } from '@/routes'
 
-const toast = useToast();
-
-const routes = [
-  { path: '/', component: ScoreCard },
-  {
-    path: "/history",
-    component: () => import("./ScoreHistory.vue")
-  },
-  {
-    path: '/diary',
-    name: 'diary',
-    component: () => import('./DiaryPage.vue')
-  },
-  {
-    path: "/data",
-    component: () => import("./DataManagement.vue"),
-    title: DataMenuItem
-  },
-  {
-    path: "/history/:id",
-    name: "viewHistory",
-    component: () => import("./ViewShoot.vue")
-  },
-  {
-    path: "/you",
-    name: "You",
-    component: () => import("./UserData.vue")
-  },
-  {
-    path: "/sight-marks",
-    name: "sight-marks",
-    component: () => import("./components/sight_marks/SightMarksPage.vue")
-  },
-  {
-    path: "/select-round",
-    name: "selectRound",
-    component: () => import("./views/RoundSelectionPage.vue"),
-    props: route => ({
-      returnTo: route.query.returnTo || "/",
-      currentRound: route.query.currentRound || ""
-    })
-  },
-  // Add the new route for the backup debugger (hidden from navigation)
-  {
-    path: '/admin/backups',
-    name: 'backupDebugger',
-    component: () => import('./components/admin/BackupDebugger.vue')
-  }
-]
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHistory(),
-  routes
-})
-
-router.beforeEach((to, from, next) => {
-  if (to.path.startsWith("/data/classifications")) {
-    return false;
-  }
-  next();
-})
+const router = createRouter()
 const intervalMS = 60 * 60 * 1000;
-
 
 if ("serviceWorker" in navigator) {
   registerSW({
---
+++ src/routes.ts
@@ -0,0 +1,66 @@
+import * as VueRouter from 'vue-router'
+import ScoreCard from './ScoreCard.vue'
+import DataMenuItem from '@/components/DataMenuItem.vue'
+
+export const routes = [
+  { path: '/', component: ScoreCard },
+  {
+    path: '/history',
+    component: () => import('./ScoreHistory.vue')
+  },
+  {
+    path: '/diary',
+    name: 'diary',
+    component: () => import('./DiaryPage.vue')
+  },
+  {
+    path: '/data',
+    component: () => import('./DataManagement.vue'),
+    title: DataMenuItem
+  },
+  {
+    path: '/history/:id',
+    name: 'viewHistory',
+    component: () => import('./ViewShoot.vue')
+  },
+  {
+    path: '/you',
+    name: 'You',
+    component: () => import('./UserData.vue')
+  },
+  {
+    path: '/sight-marks',
+    name: 'sight-marks',
+    component: () => import('./components/sight_marks/SightMarksPage.vue')
+  },
+  {
+    path: '/select-round',
+    name: 'selectRound',
+    component: () => import('./views/RoundSelectionPage.vue'),
+    props: route => ({
+      returnTo: route.query.returnTo || '/',
+      currentRound: route.query.currentRound || ''
+    })
+  },
+  {
+    path: '/admin/backups',
+    name: 'backupDebugger',
+    component: () => import('./components/admin/BackupDebugger.vue')
+  }
+]
+
+export function createRouter() {
+  const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes
+  })
+
+  router.beforeEach((to, from, next) => {
+    if (to.path.startsWith('/data/classifications')) {
+      return false
+    }
+    next()
+  })
+
+  return router
+}
